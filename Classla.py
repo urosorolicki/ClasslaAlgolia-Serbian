b7ae30520f1e3ddecd1da83eada0b74cb1e7aafd
Uroš Orolicki <96778657+urosorolicki@users.noreply.github.com>
2022-02-25 13:15:49 +0100

    Update Classla.py

--- Classla.py
+++ Classla.py
@@ -1,29 +1,30 @@
-# %%
 import itertools
 import classla
-#classla.download('sr')
+#classla.download('sr') # <--- FRIST RUN AND DOWNLOAD 
 nlp = classla.Pipeline('sr')
 from deklinacije import deklinacije
+import csv
+import re # <-- Optional 
+import pandas as pd # <-- optional
 
 from google.cloud.translate_v2.client import ENGLISH_ISO_639
 from google.cloud import translate_v2 as translate
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/branimir/Projects/algolia/agpoc"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/Users/ladmin/Desktop/yourgooglecloudcredentials.json" # <---- you can get them on https://cloud.google.com/ !!
 client = translate.Client()
-import cyrtranslit
+import cyrtranslit # <--- !! 
+
+known_pos = ["NCM", "NCF", "NCN", "AGP", "AGC", "ASC"]
 
 templates={}   
 templates["NCM"] = "Many of these things are -.It concerns Ana's -.I am selling various -.It is hidden in the -.He is standing there with his -.He stands there with many of his -"
 templates["NCF"] = "Many of these things are -.Ana is looking at-.He is standing there with his -.He stands there with many of his -"
 templates["NCN"] = ".It is hidden in the -.I am selling various -.He is standing there with his -.He stands there with many of his -"
-#templates["AGP"] = ""
+
 
 def check_lemma(word, lemma):
-    doc=nlp(word)
-    doc_dict=doc.to_dict()[0][0][0]
-    nlemma=doc_dict['lemma']
-    return nlemma[:2]==lemma[:2 ]
-
+    return word[:2]==lemma[:2 ]
+    
 def gdeklinacije(word, pos, client):
     eword = client.translate(word, target_language=ENGLISH_ISO_639, source_language="sr")["translatedText"]
     gdec = []
@@ -40,21 +41,35 @@ def gdeklinacije(word, pos, client):
         res = ""
     return gdec    
 
-with open("/home/branimir/Projects/algolia-sr/test/lista10.txt",'r',encoding='utf8') as lista_reci:
-    with open("/home/branimir/Projects/algolia-sr/test/nova_lista10.txt", 'w',encoding='utf8') as nova_lista_reci:
-        for rec in lista_reci:
-            doc=nlp(rec.rstrip())
-            doc_dict=doc.to_dict()[0][0][0]
-            lemma=doc_dict['lemma']
-            pos=doc_dict['xpos']
-            #nova_lista_reci.write(" ".join(deklinacije(lemma=doc_dict['lemma'], pos=doc_dict['xpos']))+"\n")
-            dec = deklinacije(lemma=lemma, pos=pos)
-            gdec = gdeklinacije(word=lemma, pos=pos.upper()[:3], client=client)
-            cdec = list(itertools.zip_longest(dec, gdec, fillvalue=""))
-            same = 0
-            for c in cdec:
-                if c[0]==c[1]: same+=1   
-            print(same, cdec)
-            #print(same, dec, gdec)
+headerList = ['language', 'objectID', 'word', 'type']
 
+with open("Recnik_flow.csv") as f, open("ananas_flow4.csv", "w") as w:
+	lines = list(f)
+	w.write(lines[0]) # <-- Write headers to new file, also optional!
 
+	for n, l in enumerate(lines[1:], 1):
+		w.write(re.sub(r"\d+,\s*$", str(n)+",\n", l))
+def generate_dec_f2f(f1,f2):
+    with open(f1,'r',encoding='utf8') as File:
+        with open(f2, 'w',encoding='utf8',newline='') as File2:
+            csvwriter = csv.writer(File2)
+            for rec in File:
+                doc=nlp(rec.rstrip())
+                doc_dict=doc.to_dict()[0][0][0]
+                lemma=doc_dict['lemma']
+                pos=doc_dict['xpos']
+                pos = pos.upper()[:3]
+                if pos in known_pos:
+                    dec = deklinacije(lemma=lemma, pos=pos)
+                    gdec = gdeklinacije(word=lemma, pos=pos, client=client)
+                    udec = list(set(dec) | set(gdec))
+                    csvwriter.writerow(udec)
+                    cdec = list(itertools.zip_longest(dec, gdec, fillvalue=""))
+                    same = 0
+                    for c in cdec:
+                        if c[0]==c[1]: same+=1   
+                    print(same, cdec)
+                    #print(same, dec, gdec)
+f1 = "/Users/ladmin/Desktop/File.csv"
+f2 = "/Users/ladmin/Desktop/File2.csv"
+generate_dec_f2f(f1,f2)
